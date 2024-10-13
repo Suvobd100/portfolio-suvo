1b23c1e1a49fc14a33e32afc35cdcb6a68c0cb38
suvobd100 <makbarbd@gmail.com>
2024-10-13 14:46:15 +0600

    menu added by routes

--- portfolio-suvo/src/App.jsx
+++ portfolio-suvo/src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
-import { Navbar } from './components/Navbar'
+// import {Home} from './pages/Home'
+// import './App.css'
+// import { Navbar } from './components/Navbar'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Menu from './pages/Menu'
+
+
+
+
 
 function App() {
  
   return (
-    <>
-      <Navbar/>
+   <>
+    <BrowserRouter>
+    <Menu/>
+      <Routes>
+        <Route path="/" element={<h1>Hello from route</h1>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+    
+     
     </>
   )
 }
--- portfolio-suvo/src/components/Navbar.jsx
+++ portfolio-suvo/src/components/Navbar.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-8 lg:px-14' >
-        <div className='container py-2 flex justify-end md:justify-between items-center'>
-            <div className='text-xl font-bold hidden md:inline'>
-                Akbar
-                <div className='space-x-6'>
-                    <a href="#home" className='hover:text-gray-400'>Home</a>
-                    <a href="#about" className='hover:text-gray-400'>About Me</a>
-                    <a href="#services" className='hover:text-gray-400'>Services</a>
-                    <a href="#projects" className='hover:text-gray-400'>Projects</a>
-                    <a href="#contact" className='hover:text-gray-400'>Contact</a>
-                </div>
-                <button className='bg-gradient-to-r from-green-400 to to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
-                    Contact Me
+    <nav className='bg-black text-white px-8 md:px-8 lg:px-24' >
+         
+        <div className='container py-2  px-6 flex justify-center items-center justify-between'>
+        <div className='text-2xl font-bold  md:inline'>Akbar</div>
+            <div className='space-x-6'>
+                
+                    <a href="">Home</a>
+                    <a href="">About Me</a>
+                    <a href="">Contact</a>
+                       
 
-                </button>
             </div>
+            <button>Contact Me</button>
         </div>
     </nav>
   )
